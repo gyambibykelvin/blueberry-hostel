4633419f26ef3cf6e4c384c8b31f09a4467ae97e
kelvingyambiby <kellynane001@gmail.com>
2026-04-12 10:34:02 -1200

    Update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8!sr4+_9dtbabwtprurujnxb(&=pb8=7pypaoghj=6%4t5!evc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []#'blueberry-hostel.onrender.com']
+ALLOWED_HOSTS = ['blueberry-hostel.onrender.com']
 
 
 # Application definition
