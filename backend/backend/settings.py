2c621dd1e417e95035df58fe7c696b253dea06d3
Gyambiby Anane Kelvin <kellynane001@gmail.com>
2026-04-14 22:41:28 -1200

    Set DEBUG to True in settings.py for debugging purposes
    Changed DEBUG setting from False to True for development.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8!sr4+_9dtbabwtprurujnxb(&=pb8=7pypaoghj=6%4t5!evc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['blueberry-hostel.onrender.com']
 
