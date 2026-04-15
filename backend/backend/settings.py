0eb26d4392b7d77b2ce9d27df856a474915320db
Gyambiby Anane Kelvin <kellynane001@gmail.com>
2026-04-15 03:53:46 -1200

    Integrate Cloudinary for media storage
    Added Cloudinary configuration for media storage.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+try:
 from dotenv import load_dotenv
 load_dotenv()   # This loads the .env file
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +55,15 @@ CLOUDINARY_STORAGE = {
     'SECURE': True,                 
     'MEDIA_TAG': 'media',
 }
+
+import cloudinary
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True,
+)
 
 # Use Cloudinary for media storage
 STORAGES = {
