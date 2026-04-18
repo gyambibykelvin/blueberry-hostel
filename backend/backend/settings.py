6381e4284a256db88c34803ba8e4e0df5ba114f5
kelvingyambiby <kellynane001@gmail.com>
2026-04-18 04:53:01 -1200

    Update dashboard UI

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,10 +66,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8!sr4+_9dtbabwtprurujnxb(&=pb8=7pypaoghj=6%4t5!evc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []#'blueberry-hostel.onrender.com']
 
@@ -186,8 +186,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kellynane001@gmail.com'
-EMAIL_HOST_PASSWORD = 'bqqmbkqcrkpzatmp'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'BlueBerry <noreply@blueberry.com>'
 
 # For password reset emails
